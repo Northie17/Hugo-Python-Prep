3116bb8bdabfad3e6d28522418090a21625f0ebd
Northie17 <northu2015@cranleigh.org>
2016-11-13 17:22:48 +0000

    Add files via upload

---
+++ if:elif.py
@@ -0,0 +1,9 @@
+#Hugo North Prep on If/Elif
+import random
+answer = random.randint(4,6)
+if answer == 4:
+    print("You will finally get what you have been waiting for")
+elif answer == 5:
+    print("You will make a new friend today")
+elif answer == 6:
+    print("THE END OF THE WORLD IS NEAR!!!")
