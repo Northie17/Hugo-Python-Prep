f61751f760b689798b0a586e9d9bccd03c49c4b3
Northie17 <northu2015@cranleigh.org>
2016-11-14 09:57:51 +0000

    Update Prep on if:elif.py

--- Prep on if:elif.py
+++ Prep on if:elif.py
@@ -11,5 +11,5 @@ elif answer == 4:
     print("You will finally get what you have been waiting for")
 elif answer == 5:
     print("You will make a new friend today")
-elif answer == 6:
+else:
     print("THE END OF THE WORLD IS NEAR!!!")
