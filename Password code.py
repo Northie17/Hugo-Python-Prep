9dafe8382cacb8a2b6adc033625a78c90c8376f4
Northie17 <northu2015@cranleigh.org>
2016-11-28 08:18:40 +0000

    Update Password code.py

--- Password code.py
+++ Password code.py
@@ -1,6 +1,8 @@
 Password1 = str (input ("Please enter your password:"))
 Password2 = str (input ("Please enter your password again:"))
+
 if Password1 == Password2:
     print ("Access Granted")
+    
 else:
     print ("Access Denied")
