80642ac4f771896d68b400fe5932bd69119ec429
Northie17 <northu2015@cranleigh.org>
2016-11-28 08:18:16 +0000

    Update Meal tip program.py

--- Meal tip program.py
+++ Meal tip program.py
@@ -1,8 +1,10 @@
 Name = input ("Please enter your name:")
 Meal = float (input ("Please enter cost of meal:" ))
 PercentTip = float (input ("Please enter percentage of tip :" ))
+
 Tip = Meal/PercentTip
 TotalCost = Meal + Tip
+
 print ("The total cost for your meal is £{0:.2f} as the tip is £{1:.2f}".format(TotalCost,Tip))
 print ("Thank you {0} for using this Meal Tip Calculator".format(Name))
 
